171c39a82c6b2f01f6ca1cb6b87d084b61598cd4
minsng <tjdalstmd033@naver.com>
2022-09-22 16:24:32 +0900

    0922

--- common/js/fullnav.js
+++ common/js/fullnav.js
@@ -1,109 +1,61 @@
 
 $(function() {
 
-   var smh=$('.main').height();  //비주얼 이미지의 높이를 리턴한다   900px
-   var on_off=false;  //false(안오버)  true(오버)
-       $('#headerArea').mouseenter(function(){
-          // var scroll = $(window).scrollTop();
-           $(this).css('background','#fff');
-           $('h1 a').css({'background':'url(http://minsng.cafe24.com/common/images/header_logo240100.png)'});
-           $('.dropdownmenu li a').css('color','#333'); 
-           $('.top_menu li a').css('color','#333'); 
-          
-           on_off=true;
-       });
-   
-      $('#headerArea').mouseleave(function(){
-           var scroll = $(window).scrollTop();  //스크롤의 거리
-        //    console.log(scroll);
-           if(scroll<smh-50 ){
-               $(this).css('background','none').css('border-bottom','none'); 
-               $('h1 a').css({'background':'url(http://minsng.cafe24.com/common/images/header_logo_white.png)'});
-               $('.dropdownmenu li a').css('color','#fff');
-               $('.top_menu li a').css('color','#fff'); 
-           }else{
-               $(this).css('background','#fff');
-               $('h1 a').css({'background':'url(http://minsng.cafe24.com/common/images/header_logo240100.png)'});
-               $('.dropdownmenu li a').css('color','#333');
-               $('.top_menu li a').css('color','#333'); 
-           }
-          on_off=false;    
-      });
-   
-      $(window).on('scroll',function(){//스크롤의 거리가 발생하면
-           var scroll = $(window).scrollTop();  //스크롤의 거리를 리턴하는 함수
-        //    console.log(scroll);
-        //    console.log(smh);
-        
-           if(scroll>smh-500){//스크롤300까지 내리면
-                $('#headerArea').css('background','#fff');
-                $('h1 a').css({'background':'url(http://minsng.cafe24.com/common/images/header_logo240100.png)'});
-                $('.dropdownmenu li a').css('color','#333');
-                $('.top_menu li a').css('color','#333'); 
-           }else{//스크롤 내리기 전 디폴트(마우스올리지않음)
-              if(on_off==false){
-                $('#headerArea').css('background','none').css('border-bottom','none'); 
-                $('h1 a').css({'background':'url(http://minsng.cafe24.com/common/images/header_logo_white.png)'});
-                $('.dropdownmenu li a').css('color','#fff');
-                $('.top_menu li a').css('color','#fff');
-              }
-           };            
-        });
-   
+    var smh=$('.main').height();  //비주얼 이미지의 높이를 리턴한다   900px
+    var on_off=false;  //false(안오버)  true(오버)
+    $('#headerArea').mouseenter(function(){
+        $(this).addClass('on');          
+        on_off=true;
+    });
+    $('#headerArea').mouseleave(function(){
+    var scroll = $(window).scrollTop(); // 스크롤링 리턴
+    if(scroll < smh-300){
+        $(this).removeClass('on');
+    } else {
+        $(this).addClass('on');
+    }
+    on_off = false;
+    })
+    
+    $(window).on('scroll',function(){//스크롤의 거리가 발생하면
+        var scroll = $(window).scrollTop();  //스크롤의 거리를 리턴하는 함수
+    //    console.log(scroll);
+    //    console.log(smh);
+    
+        if(scroll>smh-500){//스크롤300까지 내리면
+            $('#headerArea').addClass('on');
+        }else{
+            if(on_off==false){
+            $('#headerArea').removeClass('on');
+            }
+        };            
+    });   
 
 
     //2depth 열기/닫기
     $('ul.dropdownmenu').hover(
         function() { 
             $('ul.dropdownmenu li.menu ul').fadeIn('normal',function(){$(this).stop();}); //모든 서브를 다 열어라
-            $('ul.dropdownmenu li.menu ul').css({'height':'240px'}).clearQueue();
             $('#headerArea').animate({height:460},'fast').clearQueue();
         },function() {
             $('ul.dropdownmenu li.menu ul').hide(); //모든 서브를 다 닫아라
             $('#headerArea').animate({height:220}).clearQueue();
     });
-    
-    //1depth 효과
-    $('ul.dropdownmenu li.menu').hover(
-        function() { 
-            $('.depth1',this).css('color','#0075c1');
-        },function() {
-            $('.depth1',this).css('color','#333');
-    });
-    $('.top_menu ul li a').hover(
-        function() { 
-            $(this).css('color','#0075c1');
-        },function() {
-            $(this).css('color','#333');
-    });
 
-    //2depth hover
-    $('ul.dropdownmenu li.menu ul li a').hover(
-        function(){
-            $(this).css('color','#0075c1');            
-        },
-        function(){
-            $(this).css('color','#333');
-        }
-    );
-        
-
-     //tab 처리
-     $('ul.dropdownmenu li.menu .depth1').on('focus', function () {        
+    //tab 처리
+    $('ul.dropdownmenu li.menu .depth1').on('focus',function(){        
         $('ul.dropdownmenu li.menu ul').slideDown('normal');
-        $('#headerArea').css('background','#fff');
-        $('h1 a').css({'background':'url(http://minsng.cafe24.com/common/images/header_logo240100.png)'});
-        $('.dropdownmenu li a').css('color','#333');
-        $('.top_menu li a').css('color','#333');
         $('#headerArea').animate({height:460},'fast').clearQueue();
-     });
-
-    $('ul.dropdownmenu li.m6 li:last').find('a').on('blur', function () {        
-        $('ul.dropdownmenu li.menu ul').slideUp('fast');
-        $('#headerArea').animate({height:220},'normal').clearQueue();
-        // $('.dropdownmenu li a').css('color','#fff');
-        // $('.top_menu li a').css('color','#fff');
+        $('#headerArea').addClass('on');
     });
+    
+    $('ul.dropdownmenu li.m6 li:last').find('a').on('blur',function(){        
+        $('ul.dropdownmenu li.menu ul').slideUp('fast');
+        $('#headerArea').animate({height:220},'fast').clearQueue();
+        $('#headerArea').removeClass('on');
+    });
+
+    //topmove
     $('.topmove').click(function(e){
         e.preventDefault();
          //상단으로 스르륵 이동합니다.
@@ -165,6 +117,24 @@ $(function() {
     $('.family ul li:last a').on('blur', function () {        
             $('.family ul').fadeOut('fast');
     });
+
+
+
+            // gnb 검색 열기
+            $('#headerArea .top_menu a.searchimg').click(function(e){
+                e.preventDefault();
+                // $(".headerSearch").html(data);
+                //console.log($('#headerSearch #search'));
+                $('#headerSearch').slideDown();
+
+                // 검색 닫기
+                $('#headerSearch .close').click(function(e){
+                    e.preventDefault();
+                    $('#headerSearch').slideUp();                    
+                });
+            });
+
+
 
     
 });
--- sub1/js/tab.js
+++ sub1/js/tab.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
 
     $('.tab ul li:eq(0)').addClass('current');
-    $('.contentArea>div').hide();
-    $('.contentArea>div:eq(0)').show();
-    $('.contentArea>div:eq(1)').show();
+   
+
+    var onoff = false;
+    $(window).on('scroll',function(){ //스크롤 값의 변화가 생기면
+        var scroll = $(window).scrollTop(); //스크롤의 거리
+        
+        if(scroll>10 && onoff == false){
+            $('.contentArea>div').hide();
+            $('.contentArea>div:eq(0)').show();
+           $('.contentArea>div:eq(1)').show();
+           onoff = true;
+        }
+     });
+
 
 
     $('.tab ul a').click(function(e){
