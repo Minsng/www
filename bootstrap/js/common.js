a9985089bb97e249c5637fbd8cc670a183a38876
minsng <tjdalstmd033@naver.com>
2022-11-08 17:42:03 +0900

    1108

---
+++ bootstrap/js/common.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+
+    var swiper1 = new Swiper('.main_visual .swiper-container', {
+        loop: true,
+        navigation: {
+            nextEl: '.main_visual .swiper-button-next',
+            prevEl: '.main_visual .swiper-button-prev',
+        },
+        pagination: {
+            el: '.main_visual .swiper-pagination',
+            type: 'bullets',
+        },
+        // speed: 1,
+        autoplay: {
+            delay: 5000,
+        },
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
