4bea2cb3d628e0fd7bd4f221d52a5af5a5dd8cae
minsng <tjdalstmd033@naver.com>
2022-11-10 19:28:15 +0900

    1110

--- bootstrap/js/common.js
+++ bootstrap/js/common.js
@@ -18,7 +18,43 @@ $(document).ready(function() {
     });
 
 
-
+    var swiper2 = new Swiper("#technology .mySwiper", {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        // pagination: {
+        //     el: "#technology .swiper-pagination",
+        //     clickable: true,
+        // },
+        navigation: {
+            nextEl: "#technology .swiper-button-next",
+            prevEl: "#technology .swiper-button-prev",
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+                slidesPerGroup: 1,
+            },
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 50,
+                slidesPerGroup: 1,
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+                slidesPerGroup: 1,
+            },
+            1280: {
+                slidesPerView: 3,
+                spaceBetween: 50,
+                slidesPerGroup: 1,
+            },
+        },
+    });
 
 
 
