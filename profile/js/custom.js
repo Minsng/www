d684e7213301b43661bb832482a5474f20197d07
minsng <tjdalstmd033@naver.com>
2022-12-20 14:01:53 +0900

    1220

--- profile/js/custom.js
+++ profile/js/custom.js
@@ -1,122 +1,220 @@
 ﻿$(document).ready(function(){
 
+
+
+    // typer
+    window.onload=function(){	
+        var el       = document.querySelector('.slogan p')
+        var options  = {
+            text: '안녕하세요? 프론트엔드 개발자 & 웹퍼블리셔 송미옥입니다',
+            textSpeed: 95,
+            blinkSpeed: 0.06
+        }
+
+        var instance = new tinytyper(el, options);
+    }
+
+
+
+
     //변수 ht에 브라우저의 높이값을 저장
-    var ht = $(window).height();	
-    //브라우저의 높이값을 section의 높이값으로 지정
-    $("section").height(ht);
+    var cnt = 0;
+    var ht = $(window).height();
+    var wt = $(window).width();
+    var nowTop = cnt * ht;
+    var scroll = $(window).scrollTop();
+    var section = [];
+    var sectionLen = $('section').length;  // section의 갯수
+    var timer;
 
-    //브라우저가 리사이즈 될 때마다 브라우저와 section의 높이값을 갱신
-    $(window).on("resize",function(){
-        var ht = $(window).height();	
+    // 초기세팅
+    $("html,body").scrollTop(0); // 스크롤 맨 위로
+    
+    if(wt>1024){
         $("section").height(ht);
-    });	
+    } else {
+        $("section").height('auto');
+        $(".main").height(ht);
+    }
 
-    //각각의 section에서 마우스를 움직이면
-	$("section").on("mousemove",function(e){		
-	
-		//변수 posX에 마우스 커서의 x축 위치 저장
-		var posX= e.pageX;
-		//변수 posY에 마우스 커서의 y축 위치 저장
-		var posY= e.pageY;
-		
-		//첫 번째 박스의 이미지 위치값을 마우스 커서의 위치값과 연동
-		$(".p11").css({"right":20-(posX/30) , "bottom":20-(posY/30) });
-		$(".p12").css({"right":130+(posX/20) , "bottom":-40+(posY/20) });
-		$(".p13").css({"right":60+(posX/20) , "top":180+(posY/20) });		
-	
-		//두 번째 박스의 이미지 위치값을 마우스 커서의 위치값과 연동
-		$(".p21").css({"right":-180-(posX/30) , "bottom":-480-(posY/30) });
-		$(".p22").css({"right":130+(posX/50) , "bottom":-40+(posY/50) });
-		
-		//세 번째 박스의 이미지 위치값을 마우스 커서의 위치값과 연동
-		$(".p31").css({"right":280-(posX/30) , "bottom":30-(posY/30) });
-		$(".p32").css({"right":110+(posX/20) , "bottom":-270+(posY/20) });
-		$(".p33").css({"right":-70+(posX/20) , "bottom":-130+(posY/20) });	
-		
-		//네 번째 박스의 이미지 위치값을 마우스 커서의 위치값과 연동
-		$(".p41").css({"right":20-(posX/30) , "bottom":-120-(posY/30) });
-		$(".p42").css({"right":0+(posX/20) , "bottom":-180+(posY/20) });	
-	});
+
+
+    // scroll down
+    $('.scroll_down').on('click', function(){
+        cnt = 1;
+        // scrollMove(cnt);    // section eq1 로 이동
+        $("html,body").stop().animate({"scrollTop":section[cnt]},500);
+    })
+
+
 
     //메뉴 버튼 클릭시
-	$("#menu li").on("click",function(e){
-		e.preventDefault();
-		
-		//변수 ht에 브라우저의 높이값 저장
-		var ht = $(window).height();
-		
-		//변수 i에 현재 클릭한 li의 순서값을 저장
-		var i = $(this).index();
-		
-		//브라우저의 높이값*박스의 순서값은 현재 박스의 스크롤 위치값과 동일
-		var nowTop = i*ht;			
-	
-		//해당 스크롤 위치값으로 문서를 이동
-		$("html,body").stop().animate({"scrollTop":nowTop},1400);
-	});
+    $("#gnb li").on("click",function(e){
+        e.preventDefault();
+        cnt = $(this).index(); //변수 cnt에 현재 클릭한 li의 순서값을 저장 0 1 2 3
+        scrollMove(cnt);    // section eq (cnt)로 이동
+    });
 
-    $(window).on("scroll",function(){	
-	
-		//변수 ht에 현재 브라우저의 넓이값 저장
-		var ht = $(window).height();
-		
-		//변수 scroll에 현재 문서가 스크롤된 거리 저장
-		var scroll = $(window).scrollTop();
-		
-		/*
-		if(scroll>=ht*0 && scroll< ht*1){
-			$("#menu li").removeClass();
-			$("#menu li").eq(0).addClass("on");
-		}
-		if(scroll>=ht*1 && scroll< ht*2){
-			$("#menu li").removeClass();
-			$("#menu li").eq(1).addClass("on");
-		}
-		if(scroll>=ht*2 && scroll< ht*3){
-			$("#menu li").removeClass();
-			$("#menu li").eq(2).addClass("on");
-		}
-		if(scroll>=ht*3 && scroll< ht*4){
-			$("#menu li").removeClass();
-			$("#menu li").eq(3).addClass("on");
-		}
-		*/
-		
-		for(var i=0; i<5;i++){
-			if(scroll>=ht*i && scroll< ht*(i+1)){
-				$("#menu li").removeClass();
-				$("#menu li").eq(i).addClass("on");
-			};
-		}
-	});
 
-    //section위에서 마우스 휠을 움직이면
-    $("section").on("mousewheel",function(event,delta){    
-		
-        //마우스 휠을 올렸을때	
-        if (delta > 0) {  
-            //변수 prev에 현재 휠을 움직인 section에서 이전 section의 offset().top위치 저장
-            var prev = $(this).prev().offset().top;
-            //문서 전체를 prev에 저장된 위치로 이동
-            $("html,body").stop().animate({"scrollTop":prev},1400,"easeOutQuart");
+
+    // resize
+    $(window).on("resize",function(){
+        ht = $(window).height();
+        wt = $(window).width();
+
+        // scroll = $(window).scrollTop();
+        if(wt>1024){
+            $("section").height(ht);
+        } else {
+            $("section").height('auto');
+            $(".main").height(ht);
+        }
+
+        sectionTop();
+        scrollMove(cnt);    // section cnt로 이동
+    });
+
+
+    
+
+
+    
+    // section위에서 마우스 휠을 움직이면
+    $('section').on("mousewheel DOMMouseScroll", function(event,delta){
+
+        if (delta > 0 && cnt > 0 && wt>1024 && window.matchMedia("(min-width: 1024px)").matches) { //마우스 휠을 올렸을때
+
+            clearTimeout(timer);
+            timer = setTimeout(function(){
+                console.log(cnt);
+                cnt --;
+                scrollMove(cnt);
+            }, 150);
             return false;
+        
+        }else if (delta < 0 && cnt < sectionLen-1 && wt>1024 && window.matchMedia("(min-width: 1024px)").matches) {  //마우스 휠을 내렸을때
             
-        //마우스 휠을 내렸을때	 
-        }else if (delta < 0) {  
-            //변수 next에 현재 휠을 움직인 section에서 다음 section의 offset().top위치 저장
-            var next = $(this).next().offset().top;
-            //문서 전체를 next에 저장된 위치로 이동
-            $("html,body").stop().animate({"scrollTop":next},1400,"easeOutQuart");                                         
+            clearTimeout(timer);
+            timer = setTimeout(function(){
+                console.log(cnt);
+                cnt ++;
+                scrollMove(cnt);
+            }, 150);
             return false;
         }
     });
+
+
+
+
+    $(window).on('scroll', function(){
+
+        sectionTop();
+        for(var i=0; i<sectionLen; i++){
+            if(scroll >= section[i] && scroll < section[i]+ht){
+                cnt = i;
+                
+                $("#gnb li").removeClass();
+                $("#gnb li").eq(cnt).addClass("on");
+
+                $('article section').removeClass('active');
+                $('article section').eq(cnt).addClass('active');
+
+                // console.log(i + ' ' + cnt + ' ' + scroll + ' ' + section[i]);
+                // if(cnt == 3 || cnt == 5 ){  // mobile, bootstrap 배경 변경
+                //     $('#wrap').removeClass('dark');
+                //     $('#wrap').addClass('bright');
+                // } else {
+                //     $('#wrap').removeClass('bright');
+                //     $('#wrap').addClass('dark');
+                // }
+            }
+        }
+
+        if( $('section.mobile').hasClass('active') || $('section.bootstrap').hasClass('active')){
+            $('#wrap').removeClass('dark');
+            $('#wrap').removeClass('design');
+            $('#wrap').addClass('bright');
+
+        } else if( $('section.design').hasClass('active') ) {
+            $('#wrap').removeClass('bright');
+            $('#wrap').removeClass('dark');
+            $('#wrap').addClass('design');
+
+        } else {
+            $('#wrap').removeClass('bright');
+            $('#wrap').removeClass('design');
+            $('#wrap').addClass('dark');
+        }
+
+
+    });
+
+
+
+    // 섹션 높이값 저장
+    function sectionTop(){
+        scroll = $(window).scrollTop();
+        section = [];
+        section = $('section');
+
+        for(var i=0; i<sectionLen; i++){
+            section[i] = Math.round(section.eq(i).offset().top);
+            // console.log(section[i]);
+        }
+    }
+    sectionTop();
+
+
+    // animation
+    function scrollMove(cnt){
+        // nowTop = ht * cnt;
+        // section[cnt];
+        if(wt>1024){
+            $("html,body").stop().animate({"scrollTop":section[cnt]},500);
+            console.log('scrollMove Call');
+        }
+
+        $("#gnb li").removeClass();
+        $("#gnb li").eq(cnt).addClass("on");
+
+        $('article section').removeClass('active');
+        $('article section').eq(cnt).addClass('active');
+
+        // if(cnt == 3 || cnt == 5 ){  // mobile, bootstrap 배경 변경
+        //     $('#wrap').removeClass('dark');
+        //     $('#wrap').addClass('bright');
+        // } else {
+        //     $('#wrap').removeClass('bright');
+        //     $('#wrap').addClass('dark');
+        // }
+    }
+    scrollMove(cnt);
+
+
+
+
+
+
+    $('.gnb_open').on('click', function(e){
+        e.preventDefault();
+
+        if($('#gnb').hasClass('on')){
+            $('#gnb').removeClass('on');
+        } else {
+            $('#gnb').addClass('on');
+        }
+    })
+
+    $('#gnb li').on('click', function(){
+
+        if($('#gnb').hasClass('on')){
+            $("html,body").stop().animate({"scrollTop":section[cnt]},500);
+            $('#gnb').removeClass('on');
+        }
+
+    });
+
+
+
 });
-
-
-
-
-
-
-
-
-
