bb79f9776ae8778b266718c86304c31a469609d3
minsng <tjdalstmd033@naver.com>
2022-12-27 16:59:23 +0900

    1227

--- profile/logo.php
+++
@@ -1,9 +0,0 @@
-<h1>
-    <a href="/profile/">
-        <span>M.OK Portfolio</span>
-        <!-- <svg xmlns="http://www.w3.org/2000/svg" data-name="레이어 1" viewBox="0 0 731.22 311.15"> -->
-        <svg version="1.1" id="레이어_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="95px" height="40.125px" viewBox="0 0 95 40.125" enable-background="new 0 0 95 40.125" xml:space="preserve">
-        <ellipse cx="380.39" cy="160.5" rx="366.01" ry="154.59" style="fill:#f1c13b" transform="rotate(-2.99 278.356 441.108)"/>
-        <text style="font-size:160px;fill:#003f62;font-family:Roboto-BlackItalic,Roboto;font-weight:800;font-style:italic" transform="translate(104.95 203.79)">Minsng</text></svg>
-    </a>
-</h1>
